c44727183c10db74c098b5f8c4d2e6912c907cde
Cyrus <cy.vattes@gmail.com>
2016-03-26 15:39:50 -0700

    Added files via upload

---
+++ web-flashcards-challenge/app/controllers/decks.rb
@@ -0,0 +1,36 @@
+get '/decks' do
+  # @decks = Deck.all
+  erb :'/decks/index'
+end
+
+# get '/decks/new' do
+
+# end
+
+# post '/decks' do
+
+# end
+
+get '/decks/:id' do
+
+end
+
+get '/decks/:id/cards/:card_id' do
+
+end
+
+post '/decks/:id/cards/:card_id' do
+
+end
+
+# get '/decks/:id/edit' do
+
+# end
+
+# patch '/decks/:id' do
+
+# end
+
+# delete '/decks/:id' do
+
+# end
---
+++ web-flashcards-challenge/app/controllers/users.rb
@@ -0,0 +1,35 @@
+get '/users' do
+
+end
+
+get '/users/new' do
+
+end
+
+post '/users' do
+
+end
+
+get '/users/:id' do
+
+end
+
+get '/users/:id/rounds' do
+
+end
+
+get '/users/:id/rounds/:round_id' do
+
+end
+
+# get '/users/:id/edit' do
+
+# end
+
+# patch '/users/:id' do
+
+# end
+
+# delete '/users/:id' do
+
+# end
